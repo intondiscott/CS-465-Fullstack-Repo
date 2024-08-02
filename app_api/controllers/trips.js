8b0370619649513c672d99837ed72aaff98cd4af
Intondiscott <intondiscott@gmail.com>
2024-08-02 19:06:11 -0400

    Module 5 completed

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const Trip = require("../models/travlr");
 const Model = mongoose.model("trips");
 
-//Get Method for /trips
+//Get Method for all records /trips
 const tripsList = async (req, res) => {
   const q = await Model.find({}).exec(); //return all
+  if (!q) {
+    return res.status(404).json({ error: error });
+  } else {
+    return res.status(200).json({ trips: q });
+  }
+};
+
+//Get Method for all records /trips
+const tripsFindByCode = async (req, res) => {
+  const q = await Model.find({ code: req.params.tripCode }).exec(); //return all
 
   //debugging
   console.log(q);
@@ -16,4 +26,4 @@ const tripsList = async (req, res) => {
   }
 };
 
-module.exports = { tripsList };
+module.exports = { tripsList, tripsFindByCode };
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,35 @@
-var fs = require("fs");
-var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+//var fs = require("fs");
+//var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
 
-const travel = (req, res) =>
-  res.render("travel", { title: "Travlr Getaways", trips });
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+  method: "GET",
+  headers: {
+    "content-type": "application/json",
+  },
+};
+
+const travel = async (req, res, next) => {
+  await fetch(tripsEndpoint, options)
+    .then((data) => data.json())
+    .then((json) => {
+      let message = null;
+      if (!(json.trips instanceof Array)) {
+        message = "API lookup error";
+        json.trips = [];
+      } else {
+        if (!json.trips.length) {
+          message = "No trips exist in our database!";
+        }
+      }
+      res.render("travel", {
+        title: "Travlr Getaways",
+        trips: json.trips,
+        message,
+        selected: { travel: true },
+      });
+    })
+    .catch((err) => res.status(500).send(err.message));
+};
 
 module.exports = { travel };
